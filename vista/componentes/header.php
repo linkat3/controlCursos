dd34ee8bc4bb250dc12bf8d6b929a2d9fbbb4972
Linda Kate Lovera Fernandez <132331448+linkat3@users.noreply.github.com>
2024-06-10 14:14:16 +0100

    Add files via upload

--- vista/componentes/header.php
+++ vista/componentes/header.php
@@ -8,12 +8,13 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="stylesheet" href="http://localhost/Programacion/TercerTrimestre/controlCursos/controlCursos/vista/estilos/estilo.css">
-    <link rel="stylesheet" href="<?php echo $url_base; ?>vista/estilos/features.css">
-    <link rel="stylesheet" href="<?php echo $url_base; ?>vista/estilos/signin.css">
-
+    <link rel="stylesheet" href="http://localhost/Programacion/TercerTrimestre/controlCursos/controlCursos/vista/estilos/features.css">
+    <link rel="stylesheet" href="http://localhost/Programacion/TercerTrimestre/controlCursos/controlCursos/vista/estilos/signin.css">
+    <link rel="stylesheet" href="http://localhost/Programacion/TercerTrimestre/controlCursos/controlCursos/vista/estilos/intro.css">
     <!-- Bootstrap CSS v5.2.1 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <!-- icons bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
 <body data-bs-theme="light">
