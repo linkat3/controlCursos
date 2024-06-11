d1603f8b84341b4e3f04ba78262dab8e05629ea5
Linda Kate Lovera Fernandez <132331448+linkat3@users.noreply.github.com>
2024-06-11 22:17:07 +0100

    Add files via upload

--- vista/componentes/header.php
+++ vista/componentes/header.php
@@ -1,4 +1,4 @@
-<?php $url_base = "http://localhost/Programacion/TercerTrimestre/controlCursos/controlCursos/";  ?>
+<?php $url_base = "http://localhost/MajadaP/controlCursos/";  ?>
 <!doctype html>
 <html lang="es">
 
@@ -7,10 +7,10 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="stylesheet" href="http://localhost/Programacion/TercerTrimestre/controlCursos/controlCursos/vista/estilos/estilo.css">
-    <link rel="stylesheet" href="http://localhost/Programacion/TercerTrimestre/controlCursos/controlCursos/vista/estilos/features.css">
-    <link rel="stylesheet" href="http://localhost/Programacion/TercerTrimestre/controlCursos/controlCursos/vista/estilos/signin.css">
-    <link rel="stylesheet" href="http://localhost/Programacion/TercerTrimestre/controlCursos/controlCursos/vista/estilos/intro.css">
+    <link rel="stylesheet" href="http://localhost/MajadaP/controlCursos/vista/estilos/estilo.css">
+    <link rel="stylesheet" href="http://localhost/MajadaP/controlCursos/vista/estilos/features.css">
+    <link rel="stylesheet" href="<?php echo $url_base; ?>vista/estilos/signin.css">
+    <link rel="stylesheet" href="<?php echo $url_base; ?>vista/estilos/intro.css">
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <!-- icons bootstrap -->
@@ -25,10 +25,13 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" href="http://localhost/Programacion/TercerTrimestre/controlcursos/controlcursos/index.php" aria-current="page">Cursos <span class="visually-hidden">(current)</span></a>
+                            <a class="nav-link active" href="http://localhost/MajadaP/controlCursos/index.php" aria-current="page">Cursos <span class="visually-hidden">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="<?php echo $url_base; ?>modelo/">El Centro</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo $url_base; ?>tienda_libros.php">Tienda de Libros</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="<?php echo $url_base; ?>contacto.php">Formulario de Contacto</a>
@@ -45,3 +48,4 @@
     <main class="container">
         <br>
         <br>
+        
