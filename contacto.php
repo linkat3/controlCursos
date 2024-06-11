e1be840c603857fafe1ce0f19c0a2bc8e324ab83
Linda Kate Lovera Fernandez <132331448+linkat3@users.noreply.github.com>
2024-06-11 22:14:07 +0100

    Add files via upload

--- contacto.php
+++ contacto.php
@@ -21,4 +21,4 @@
 <div class="d-flex justify-content-center align-items-center m-2">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3337.004497938251!2d-13.86965622482475!3d28.51238648952706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc47c62bf0f8b75b%3A0xd77378a1756f2606!2sCIFP%20Majada%20Marcial!5e1!3m2!1ses!2ses!4v1716278562350!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
-<?php include("vista/componentes/footer.php") ?>
+<?php include "./vista/componentes/footer.php" ?>
