8971f756841bb523553352c7f7652c65c5a99c4e
Linda Kate Lovera Fernandez <132331448+linkat3@users.noreply.github.com>
2024-06-11 13:39:20 +0100

    Add files via upload

--- modelo/editar_nota.php
+++ modelo/editar_nota.php
@@ -1,8 +1,12 @@
 <?php
 include_once "conectar.php";
 include_once "../controlador/Notas.php";
-$nota = new Nota($_POST["nota"], $_POST["idEstudiante"], $_POST["modulo"], $_POST["tipo"]);
-$nota->actualizar();
-header("Location: ../vista/asigna_nota_materia.php?id=" . $_GET["modulo"]);
+$nota = new Nota($_POST["puntaje"],$_POST["id_estudiante"], $_POST["tipo"],$_POST["id_materia"]);
+$nota->guardar();
+header("Location: ../vista/asigna_nota_materia.php?id=" . $_GET["id_materia"]);
 
 
+// $this->puntaje = $puntaje;
+//         $this->idEstudiante = $idEstudiante;
+//         $this->tipo = $tipo;
+//         $this->idMateria = $idMateria;
